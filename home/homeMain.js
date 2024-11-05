00fd607897cc80704650ef0a9256acc7b08254eb
Unkn0wn-D3v <kimc1145@oshkosh.k12.wi.us>
2024-11-04 20:46:31 -0600

    Create homeMain.js

---
+++ home/homeMain.js
@@ -0,0 +1,35 @@
+const signoutButton = document.getElementById("sign-out");
+
+var pageOpenedOnce = localStorage.getItem("pageOpenedOnce");
+
+switch (pageOpenedOnce) {
+  case "true":
+    pageOpenedOnce = true;
+    break;
+  case "false":
+    pageOpenedOnce = false;
+    break;
+  default:
+    pageOpenedOnce = false;
+}
+
+if (!pageOpenedOnce) {
+  pageOpenedOnce = true;
+  signedIn = true;
+}
+
+if (!signedIn) {
+  window.location.replace("/../index.html");
+}
+
+signoutButton.addEventListener("click", () => {
+  localStorage.clear();
+  window.location.replace("/../index.html");
+});
+
+window.onbeforeunload = onUnload;
+
+function onUnload() {
+  if (!isSaving) signedIn = false;
+  localStorage.setItem("pageOpenedOnce", pageOpenedOnce);
+}
